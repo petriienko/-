100ec7c97a0ed35e92c24c5c0cb0f8d6c3810061
petriienko <113470569+petriienko@users.noreply.github.com>
2022-09-13 22:42:44 +0300

    Update Курс біткоін.py

--- Курс біткоін.py
+++ Курс біткоін.py
@@ -1,5 +1,5 @@
 print("What is Bitcoin price today?")
 a = int(input()) #Ціна біткоіна
 print("How much $ do you have?")
-b = int(input()) #Кількість біткоінів
+b = int(input()) #Кількість USD
 print("You can buy", b / a, "BTC")
